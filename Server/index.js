50bd363f04870663ff072f8b5d6fcf22da4f6dfd
Lee Hyun <ink0416@naver.com>
2019-10-20 09:29:56 +0900

    edit seoaflake

--- Server/index.js
+++ Server/index.js
@@ -70,14 +70,18 @@ ws.on('connection', (client) => {
 })
 
 // 임시적으로 여기에 둠
-const seoaflake = () => {
-  const now = new Date().getTime()
-  const random = Math.floor(Math.random() * 89999 + 10000)
-  return now.toString(2) + random.toString(2)
+class Seoaflake {
+  constructor () {
+    this.createData = new Date().getTime()
+    let temp = `${Math.floor(Math.random() * 89999 + 10000).toString(2)}`
+    for (let i = 0; i < 17 - temp.toString(2).length; i++) temp = `0${temp}`
+    this.id = parseInt(this.createData.toString(2) + temp, 2)
+    this.rand = parseInt(temp, 2)
+  }
 }
 
 class User {
   constructor (id) {
-    this.id = id || seoaflake()
+    this.id = id || new Seoaflake()
   }
 }
